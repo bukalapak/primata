ab7bc01d37f464e2ab520d1d484e8120afe36254
Alif Rachmawadi <subosito@gmail.com>
2014-09-21 22:22:11 +0700

    no need to set default country

--- lib/primata/sms.rb
+++ lib/primata/sms.rb
@@ -1,12 +1,10 @@
 require 'twilio-ruby'
 require 'phonelib'
 
-Phonelib.default_country = 'ID'
-
 module Primata
   class SMS
     def self.normalized_number(number)
-      number = Phonelib.parse(number.to_s)
+      number = Phonelib.parse(number.to_s, 'ID')
       return if number.invalid?
 
       number.international.gsub(/[^+0-9]+/, '')
